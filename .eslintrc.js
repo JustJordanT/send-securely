e973613c82754a4a12235d3a6ef76bec00347abf
JT <Jordantay9014@outlook.com>
2023-05-11 16:55:58 -0500

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  root: true,
-  extends: ['@basis-theory/eslint-config/next'],
-  ignorePatterns: ['load-tests/*.js'],
+    root: true,
+    extends: ['@basis-theory/eslint-config/next'],
+    ignorePatterns: ['load-tests/*.js'],
+    rules: {
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off"
+    }
 };
