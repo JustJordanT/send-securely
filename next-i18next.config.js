60e754c1dba572f9103e27cb9809289a72b7f784
JT <Jordantay9014@outlook.com>
2023-06-02 16:18:36 -0500

    testing

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'pt-BR'],
+    locales: ['en'],
     react: {
       useSuspense: false,
     },
